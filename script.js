dc9f3eec9c7db8e1cba4a37647ed51b3dc42cefd
shev-andry <136947471+shev-andry@users.noreply.github.com>
2023-08-08 00:30:53 +0700

    make a function that darken the pixel everytime you hover on it

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ const borderBtn = document.querySelector(".btn-border");
 const canvas = document.querySelector("#canvas");
 slider.addEventListener("input", changeCanvas);
 resetBtn.addEventListener("click", changeCanvas);
-borderBtn.addEventListener('click', toggleBorder)
+borderBtn.addEventListener("click", toggleBorder);
 // TODO
 // shift + hover = color white
 // random color
-// darkening mode 
+// darkening mode
 generatePixel();
-rainbowMode()
+rainbowMode();
+darkenMode();
 
 function resetCanvas() {
   while (canvas.hasChildNodes()) {
@@ -22,6 +23,8 @@ function resetCanvas() {
 function changeCanvas() {
   resetCanvas();
   generatePixel();
+  rainbowMode();
+  darkenMode();
 }
 
 function generatePixel() {
@@ -29,7 +32,7 @@ function generatePixel() {
   let pixelSize = canvasHeight / totalPixelSqrt + "px";
   for (let i = 0; i < totalPixelSqrt; i++) {
     const pixelFlex = document.createElement("div");
-    pixelFlex.classList.add('divflex')
+    pixelFlex.classList.add("divflex");
     pixelFlex.style.display = "flex";
     canvas.appendChild(pixelFlex);
     for (let i = 0; i < totalPixelSqrt; i++) {
@@ -39,25 +42,27 @@ function generatePixel() {
         pixel.style.width = width;
         pixel.style.height = height;
       }
-      pixel.classList.add('border-on')
+      pixel.classList.add("border-on");
       pixel.style.backgroundColor = "white";
       pixel.style.boxSizing = "border-box";
       pixelFlex.appendChild(pixel);
     }
   }
   changeColor();
-  changeSliderNum()
+  changeSliderNum();
 }
 
 function changeColor() {
   const pixel = document.querySelectorAll("#canvas .divflex div");
   for (let i = 0; i < pixel.length; i++) {
     pixel[i].addEventListener("mouseover", (e) => {
-      if (e.shiftKey === true) {
-        pixel[i].style.backgroundColor = 'white';
-        return
+      if (e.shiftKey === true && e.altKey === false) {
+        pixel[i].style.backgroundColor = "white";
+        return;
       }
-      pixel[i].style.backgroundColor = getColor();
+      if (e.altKey === false){
+        pixel[i].style.backgroundColor = getColor();
+      }
     });
   }
 }
@@ -67,24 +72,12 @@ function getColor() {
   return colorPicker.value;
 }
 
-function changeSliderNum() {
-  const sliderNum = document.querySelector('.slider p')
-  sliderNum.textContent = `${slider.value} x ${slider.value}`
-}
-
-function toggleBorder() {
-  const pixel = document.querySelectorAll("#canvas .divflex div");
-  for (let i = 0; i < pixel.length; i++) {
-    pixel[i].classList.toggle('border-on')
-  }
-}
-
 function rainbowMode() {
   const pixel = document.querySelectorAll("#canvas .divflex div");
   for (let i = 0; i < pixel.length; i++) {
     pixel[i].addEventListener("mouseover", (e) => {
       if (e.ctrlKey === false) {
-        return
+        return;
       }
       pixel[i].style.backgroundColor = getRandomColor();
     });
@@ -92,5 +85,34 @@ function rainbowMode() {
 }
 
 function getRandomColor() {
-  return `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`
+  return `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(
+    Math.random() * 255
+  )}, ${Math.floor(Math.random() * 255)})`;
+}
+
+function darkenMode() {
+  const pixel = document.querySelectorAll("#canvas .divflex div");
+  for (let i = 0; i < pixel.length; i++) {
+    pixel[i].addEventListener("mouseover", (e) => {
+      if (e.altKey === false) {
+        return;
+      }
+      const compStyle = window.getComputedStyle(pixel[i]);
+      const colorValueRGB = compStyle.getPropertyValue('background-color');
+      const colorArr = colorValueRGB.substring(4, colorValueRGB.length-1).replace(/ /g, '').split(',');
+      pixel[i].style.backgroundColor = `rgb(${colorArr[0]-25.5},${colorArr[1]-25.5},${colorArr[2]-25.5})`
+    });
+  }
+}
+
+function changeSliderNum() {
+  const sliderNum = document.querySelector(".slider p");
+  sliderNum.textContent = `${slider.value} x ${slider.value}`;
+}
+
+function toggleBorder() {
+  const pixel = document.querySelectorAll("#canvas .divflex div");
+  for (let i = 0; i < pixel.length; i++) {
+    pixel[i].classList.toggle("border-on");
+  }
 }
